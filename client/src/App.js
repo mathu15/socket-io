ad852a36f338ecd975aafe64166c6e9c3590832e
mathu15 <mathumitha1509@gmail.com>
2023-06-17 15:46:57 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const [room, setRoom] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      // alert(data.message);
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      ></input>
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+  );
+}
+
+export default App;
